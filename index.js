9a9b93e33841a7aca35fab141bb2d92e8e7177c3
yogendrapatidar <88493200+yogendrapatidar@users.noreply.github.com>
2023-05-19 23:44:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// toggle icon navbar
+
+let menuicon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuicon.onclick=()=>{
+    menuicon.classList.toggle('bx');
+    navbar.classList.toggle('active');
+}
+
+
+// scroll section active links
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset= sec.offsetTop-150;
+        let height= sec.offsetHeight;
+        let id= sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+ 
+        };
+    });
+    // ============Sticky navbar===========
+
+let header=document.querySelector('header');
+
+header.classList.toggle('sticky',window.scrollY>100);
+// ===========remove toggle icon and anvbar when click navbar link==========
+    menuicon.classList.remove('bx');
+    navbar.classList.remove('active');
+
+
+};
+
+
+
+
